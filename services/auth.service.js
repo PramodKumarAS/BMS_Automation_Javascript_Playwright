3018aa5d109ea2c01f5dfb5665371ca5873ae32e
Pramod Kumar <pramodkumaras143@gmail.com>
2026-03-11 09:15:04 +0530

    Add user registration, password reset, and current user retrieval functionalities; implement API schema validations and tests

--- services/auth.service.js
+++ services/auth.service.js
@@ -19,6 +19,69 @@ class AuthApiService{
 
         return response;
     }
+
+    async register(email){
+        const endpoint = `${process.env.API_BASE_URL}/api/user/register`;
+
+        const response = await this.request.post(endpoint,{
+            headers:{
+                'Content-Type':'application/json'
+            },
+            data:{
+                email:email,
+                name:"Pramod Kumar A S",
+                password:"12344"
+            }
+        });
+
+        return response;
+    }
+
+    async forgetPassword(email){
+        const endpoint = `${process.env.API_BASE_URL}/api/user/forgetpassword`;
+
+        const response = await this.request.post(endpoint,{
+            headers:{
+                'Content-Type':'application/json'                
+            },
+            data:{
+                email:email
+            }
+        });
+
+        return response;
+    }
+
+    async resetPassword(otp,password){
+        const endpoint = `${process.env.API_BASE_URL}/api/user/resetpassword`;
+
+        const response = await this.request.post(endpoint,{
+            headers:{
+                'Content-Type':'application/json'                
+            },
+            data:{
+                otp:otp,
+                password:password
+            }
+        });
+
+        return response;
+    }
+
+    async getCurrentUser(token){
+        const endpoint = `${process.env.API_BASE_URL}/api/user/get-currentUser`;
+
+        const response = await this.request.get(endpoint,{
+            headers:{
+                'Content-Type':'application/json',
+                Authorization:`Bearer ${token}`            
+            }
+        });
+
+        return response;
+    }
 }
 
-module.exports={AuthApiService};
+module.exports={
+    AuthApiService
+};
---
+++ tests/api/user.api.spec.js
@@ -0,0 +1,256 @@
+import { test, expect } from '@playwright/test';
+import { AuthApiService } from '../../services/auth.service';
+import Ajv from 'ajv';
+import { findMongoRecord, findMongoRecordById, MongoConnect } from '../../services/mongo.service';
+
+test('POST - Login API Schema validation',async({request})=>{
+    const authAPI = new AuthApiService(request);
+
+    const apiResponse = await authAPI.login(process.env.USER_EMAIL,process.env.PASSWORD);
+    await expect(apiResponse.status()).toBe(200);
+
+    const data = await apiResponse.json();
+    
+    const schema = {
+    type: "object",
+    required: ["success", "token", "message", "role"],
+    properties: {
+        success: { type: "boolean" },
+        token: { type: "string" },
+        message: { type: "string" },
+        role: { type: "string", enum: ["User"] }
+    },
+    additionalProperties: false
+    };
+
+    const ajv = new Ajv();
+    const validate = ajv.compile(schema);
+    const valid = validate(data);   
+
+    expect(valid).toBe(true);    
+});
+
+test('POST - Login API should return 200 Success for valid credentials',async({request})=>{
+    const authApiService =new AuthApiService(request);
+    const apiResponse = await authApiService.login(process.env.USER_EMAIL,process.env.PASSWORD);
+    const data = await apiResponse.json();
+   
+    expect(apiResponse.status()).toBe(200);
+    expect(data.success).toBe(true);
+    expect(data.token).not.toBeNull();
+    expect(data.message).toBe('Successfully logged in!');
+});
+
+test('POST - Login API should return 404 Not Found for Invalid credentials',async({request})=>{
+    const authApiService =new AuthApiService(request);
+    const apiResponse = await authApiService.login("user@gmail.com","102806");
+    const data = await apiResponse.json();
+
+    expect(apiResponse.status()).toBe(404);
+    expect(data.success).toBe(false);
+    expect(data.message).toBe('No user found');
+});
+
+test('POST - Register API Schema Validation',async({request})=>{
+    const apiAuth = new AuthApiService(request);
+
+    const apiResponse = await apiAuth.register('pkUser2@gmail.com');
+    expect(apiResponse.status()).toBe(200);
+    const data  = await apiResponse.json();
+
+    const schema = {
+        type:"object",
+        required:["success", "message"],
+        properties: {
+        success: { type: "boolean" },
+        message: { type: "string" }
+        },
+        additionalProperties: false
+    };
+
+    const ajv = new Ajv();
+    const validate = ajv.compile(schema);
+    const valid = validate(data);
+    expect(valid).toBe(true);
+});
+
+test('POST - Register API should return 200 Success for valid Email',async({request})=>{
+    const authAPI = new AuthApiService(request);
+    const apiResponse=await authAPI.register("pkUser1@gmail.com");
+
+    expect(apiResponse.status()).toBe(200)
+
+    const data = await apiResponse.json();
+
+    expect(data.success).toBe(true);
+    expect(data.message).toBe('Registration is successfull');
+});
+
+test('POST - Register API should return 400 Bad Request for Invalid Email Domain',async({request})=>{
+    const apiAuth = new AuthApiService(request);
+    const apiResponse = await apiAuth.register("pkUser@mail.com");
+
+    expect(apiResponse.status()).toBe(400);
+});
+
+test('POST - Foget Password API Schema Validation',async({request})=>{
+    const apiAuth = new AuthApiService(request);
+    const apiResponse = await apiAuth.forgetPassword(process.env.USER_EMAIL);
+    await expect(apiResponse.status()).toBe(200);
+
+    const data = await apiResponse.json();
+
+    const schema = {
+        type:"object",
+        required:["status","message"],
+        properties:{
+            status:{type:"string"},
+            message:{type:"string"}
+        },
+        additionalProperties:false
+    };
+
+    const ajv = new Ajv();
+    const validate = ajv.compile(schema);
+    const valid = validate(data);
+
+    expect(valid).toBe(true);
+});
+
+test('POST - Forget Password API should return 200 Success for the Valid User',async({request})=>{
+    const authAPI = new AuthApiService(request);
+
+    const apiResponse = await authAPI.forgetPassword(process.env.USER_EMAIL);
+    await expect(apiResponse.status()).toBe(200);
+
+    const data = await apiResponse.json();
+    expect(data.status).toBe("success");
+    expect(data.message).toBe("otp sent to your email");
+});
+
+test('POST - Forget Password API should return 404 Not found error for Invalid User',async({request})=>{
+    const apiAuth = new AuthApiService(request);
+    const apiResponse = await apiAuth.forgetPassword("pkuser133@gmail.com");
+
+    await expect(apiResponse.status()).toBe(404);
+
+    const data = await apiResponse.json();
+    expect(data.status).toBe("failure");
+    expect(data.message).toBe("user not found for this email");
+});
+
+test('POST - Reset Password API schema validation',async({request})=>{
+    const apiAuth = new AuthApiService(request);
+    await apiAuth.forgetPassword(process.env.USER_EMAIL);
+   
+    await MongoConnect("test","users");
+    const mdbResposne = await findMongoRecord(process.env.USER_EMAIL);
+   
+    const apiResponse = await apiAuth.resetPassword(mdbResposne.otp,"14036");
+    await expect(apiResponse.status()).toBe(200);
+
+    const data = await apiResponse.json();
+
+    const schema={
+        type:"object",
+        required:["status","message"],
+        properties:{
+            status:{type:"string"},
+            message:{type:"string"}
+        },
+        additionalProperties:false
+    };
+
+    const ajv = new Ajv();
+    const validate = ajv.compile(schema);
+    const valid = validate(data);
+
+    expect(valid).toBe(true);
+});
+
+test('POST - Reset Password API should return 200 success for Valid User',async({request})=>{
+    const apiAuth = new AuthApiService(request);
+    await apiAuth.forgetPassword(process.env.USER_EMAIL);
+   
+    await MongoConnect("test","users");
+    const mdbResposne = await findMongoRecord(process.env.USER_EMAIL);
+   
+    const apiResponse = await apiAuth.resetPassword(mdbResposne.otp,"14036");
+    await expect(apiResponse.status()).toBe(200);
+
+    const data = await apiResponse.json();
+    expect(data.status).toBe("success");
+    expect(data.message).toBe("password reset successfully");
+});
+
+test('POST - Reset Password should return 400 Bad Request for wrong Otp',async({request})=>{
+    const apiAuth = new AuthApiService(request);
+    const apiResponse = await apiAuth.resetPassword("1233","14333");
+    await expect(apiResponse.status()).toBe(400);
+
+    const data = await apiResponse.json();
+    expect(data.status).toBe('failure');
+    expect(data.message).toBe('OTP is wrong');
+});
+
+test('GET - Get Current User API should return 200 success for Valid User',async({request})=>{
+    const apiAuth = new AuthApiService(request);
+    const loginAPIResponse =await apiAuth.login(process.env.USER_EMAIL,process.env.PASSWORD);
+    await expect(loginAPIResponse.status()).toBe(200);
+
+    const loginData = await loginAPIResponse.json();
+    const apiResponse = await apiAuth.getCurrentUser(loginData.token);
+    await expect(apiResponse.status()).toBe(200);
+
+    const data = await apiResponse.json();
+    expect(data.success).toBe(true);
+    expect(data.user._id).not.toBeNull();
+    expect(data.user.name).toBe("UserPramod");
+    expect(data.user.email).toBe(process.env.USER_EMAIL);
+    expect(data.user.role).toBe("User");
+});
+
+test('GET - Get Current User API Schema Validation',async({request})=>{
+    const apiAuth = new AuthApiService(request);
+    const loginAPIResponse =await apiAuth.login(process.env.USER_EMAIL,process.env.PASSWORD);
+    await expect(loginAPIResponse.status()).toBe(200);
+
+    const loginData = await loginAPIResponse.json();
+    const apiResponse = await apiAuth.getCurrentUser(loginData.token);
+    await expect(apiResponse.status()).toBe(200);
+
+    const data = await apiResponse.json();
+
+    const schema = {
+        type: "object",
+        properties: {
+            success: { type: "boolean" },
+            user: {
+            type: "object",
+            properties: {
+                _id: { type: "string" },
+                name: { type: "string" },
+                email: { type: "string" },
+                role: { type: "string" },
+                __v: { type: "number" }
+            },
+            required: ["_id", "name", "email", "role"]
+            }
+        },
+        required: ["success", "user"]
+    };
+
+    const ajv = new Ajv();
+    const validate= ajv.compile(schema);
+    const valid = validate(data);
+
+    expect(valid).toBe(true);
+    
+});
+
+test('GET - Get current User API should return 401 Unauthorized for invalid token',async({request})=>{
+    const apiAuth=new AuthApiService(request);
+    const apiResponse = await apiAuth.getCurrentUser("");
+
+    await expect(apiResponse.status()).toBe(401);
+}); 
